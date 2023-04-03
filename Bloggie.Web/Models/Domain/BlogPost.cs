e130822019e74614ea1da133e9e1e628560fe0c7
Sidney Shafer <sidney@mnshafers.com>
2023-04-03 07:55:32 -0500

    Add files via upload

---
+++ Bloggie.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,23 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        //Properties for BlogPost
+        public Guid Id { get; set; } //Guid - unique identifier
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+
+        // relationship between blog post and tag is defined
+        // Navigation property
+        public ICollection<Tag> Tags { get; set; }
+        public ICollection<BlogPostLike> Likes { get; set; }
+        public ICollection<BlogPostComment> Comments { get; set; }
+    }
+}
---
+++ Bloggie.Web/Models/Domain/Tag.cs
@@ -0,0 +1,12 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+
+        // relationship between blog post and tag is defined
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
